f03468e2e57a264710be4a77846b525d80293623
zaman <Areeb.5.Zaman@niit.com>
2023-05-25 00:01:37 +0530

    structure created

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appID ="1:687544141835:web:d3fe3609119a7b4b985f0e";
+  static String apiKey="AIzaSyAdxRO6QdMFEUvY8h__3igFU4fNaKExJv4";
+  static String messagingSenderId="687544141835";
+  static String projectId="hatnet-c1d22";
+}
